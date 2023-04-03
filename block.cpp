f0dc3cb693ff635eba05fe9a023d1dbcc8b8f3cd
Oucheoma <105271781+Oucheoma@users.noreply.github.com>
2023-04-03 08:59:52 -0700

    Create block.cpp

---
+++ block.cpp
@@ -0,0 +1,32 @@
+//My name is ucheoma chioma 
+#include <iostream>
+
+int main (){
+  std::cout << "u";     std::cout << "       u\n";
+  std::cout << "u";     std::cout << "       u\n";
+  std::cout << "u";     std::cout << "       u\n";
+  std::cout << "u";     std::cout << "       u\n";
+  std::cout << "u";     std::cout << "       u\n";
+  std::cout << "u";     std::cout << "       u\n";
+  std::cout << "  u";
+  std::cout << "  u";
+  std::cout << "  u/n";
+
+
+
+  std::cout << "  c\n";
+  std::cout << "  c";
+  std::cout << "  c";
+  std::cout << "  c\n";
+  std::cout << "c\n";
+  std::cout << "c\n";
+  std::cout << "c\n";
+  std::cout << "c\n";
+  std::cout << "c\n";
+  std::cout << "c\n";
+  std::cout << "  c";
+  std::cout << "  c";
+  std::cout << "  c\n";
+
+
+}
